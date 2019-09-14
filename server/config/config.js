5a572c52135dedfd36df26ad12d09cd0685b75ff
xurxovega <dariovegasilva@gmail.com>
2019-09-14 14:31:43 +0200

    var URI

--- server/config/config.js
+++ server/config/config.js
@@ -8,10 +8,10 @@ process.env.NODE_ENV = process.NODE_ENV || 'dev';
 
 let urlDB = '';
 
-//if (process.env.NODE_ENV === 'dev'){
-//    urlDB = 'mongodb://localhost:27017/cafe';
-//}else{
-    urlDB = 'mongodb://admin:admin1@ds223578.mlab.com:23578/cafe_xvs';
-//}
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
