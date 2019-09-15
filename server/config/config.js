30525f1776a9e1af5e0e5cbbdde711f5ac3ef00b
xurxovega <dariovegasilva@gmail.com>
2019-09-15 16:44:42 +0200

    Change on Process.env.NODE_ENV

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
 process.env.PORT = process.env.PORT || 3000;
 
 //Entorno
-process.env.NODE_ENV = process.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Variable for DB
 let urlDB = '';
